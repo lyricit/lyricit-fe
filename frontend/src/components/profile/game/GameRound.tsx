d05906f7734a04c1f881b1ff805f2f29a2d32322
kimgiraffe <kimgiraffe@naver.com>
2024-03-29 15:38:34 +0900

    style: refactor game page layout and styles

--- frontend/src/components/profile/game/GameRound.tsx
+++ frontend/src/components/profile/game/GameRound.tsx
@@ -1,6 +1,6 @@
 export default function GameRound({ round }: { round: number }) {
   return (
-    <span className="flex h-10 w-full select-text items-center justify-center rounded-[10px] bg-pink-400 font-bold text-2xl text-white">
+    <span className="flex h-20 w-full select-text items-center justify-center rounded-[10px] bg-pink-400 font-bold text-2xl text-white">
       ROUND {round}
     </span>
   );
